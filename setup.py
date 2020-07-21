83b4895931fd9f536b6e072f333f44fb35d17837
Samuel.Wu <wyh@ivysci.com>
2020-07-21 03:55:14 +0000

    Use setuptools insteads of distutils

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 
 with open("README.md", "r") as fh:
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pygrobid",
-    version="0.0.3",
+    version="0.1.1",
     author="Samuel.Wu",
     author_email="samuel.yh.wu@gmail.com",
     description="A python client for Grobid service",
